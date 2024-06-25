dfede11f4fdf9610173c9cd91304fc9c4206e3dc
nhango2023 <ngothanhnha2017@gmail.com>
2024-06-25 10:33:45 +0700

    replace file upload with code editor

--- Back-end/script.js
+++ Back-end/script.js
@@ -1,3 +1,279 @@
+const BAI1 = `moduleBAI1(Y,X);outputY;inputX;assignY=X;endmodulemoduleBAI1test(outputwire(0:0)LEDR,inputwire(0:0)SW);BAI1DUT(.Y(LEDR[0]),.X(SW[0]));endmodule`
+
+const BAI2A = `moduleBAI2A(outputwireY,inputwireA,inputwireB);assignY=A&B;endmodulemoduleBAI2A_test(outputwire[1:0]LEDR,outputwire[0:0]LEDG,inputwire[1:0]SW);assignLEDR[1:0]=SW[1:0]BAI2ADUT(.Y(LEDG[0]),.A(SW[0]),.B(SW[1]));endmodule`
+
+const BAI3A = `moduleBAI3A(outputwireSUM,outputwireCARRY,inputwireA,inputwireB);assignSUM=A^B;assignCARRY=A&B;endmodulemoduleBAI3A_test(output[1:0]LEDR,output[1:0]LEDG,input[1:0]SW);assignLEDR[1:0]=SW[1:0];BAI3ADUT(.SUM(LEDG[1]),CARRY(LEDG[0]),.A(SW[0]),.B(SW[1]));endmodule`
+
+const BAI4A = `moduleBAI4A(outputregQ,outputwireQbar,inputwireD,inputwireclk,inputwirerst);assignQbar=~Q;always@(posedgeclk)beginif(~rst)Q=1'b0;elseQ=D;endendmodulemoduleBAI4A_test(output[0:0]LEDR,output[1:0]LEDG,input[0:0]SW,input[1:0]KEY);assignLEDR[0]=SW[0];BAI4ADUT(.Qbar(LEDG[0]),.D(SW[0]),.Q(LEDG[1]),.clk(KEY[1]),.rst(KEY[0]));endmodule`
+
+const BAI5A = `moduleBAI5A(output[3:0]Q,inputclk,inputrst);reg[3:0]value;always@(posedgeclk)beginif(~rst)value<=4'd0;elsevalue<=value+4'd1;endassignQ=value;endmodulemoduleBAI5A_test(output[3:0]LEDR,output[1:0)LEDG,input[1:0]KEY);assignLEDG[1:0]=KEY[1:0]BAI5ADUT(.Q(LEDR[3:0]),.clk(KEY[1]),.rst(KEY[0]));endmodule`
+
+const BAI6A = `moduleBAI6A(inputwirex,inputwirerst,inputwireclk,outputregw);parameter[2:0]A=3'b000,B=3'b001,C=3'b010,D=3'b011,E=3'b100;reg[2:0]p_state,n_state;always@(xorp_state)beginn_state=Acase(p_state)A:if(x==1'b1)n_state=B;elsenstate=A;B:if(x==1'b1)n_state=B;elsen_state=C;C:if(x==1'b1)n_state=Delsen_state=A;D:if(x==1'b1)n_state=E;elsen_state=C;E:if(x==1'b1)n_state=B;elsen_state=Cdefault:n_state=Aendcaseendalways@(xorp_state)beginw=1'b0;w=(p_state==E);endalways@(posedgeclk)beginif(~rst)p_state=Aelsep_state=n;endendmodule`
+
+// switch
+const switchInputs = document.querySelectorAll('.switch-input')
+
+//led
+const ledOutput = document.querySelectorAll('.led')
+
+// Output of complie verilog code
+var output = document.getElementById('Output');
+
+//Code editor
+const textArea = document.getElementById("code");
+
+//Set variable for setInterval to computing I/O 
+var Io_computing;
+
+
+//function for line count
+function updateLineCount() {
+
+    const lineCountElement = document.getElementById("lines");
+
+    // Get the number of lines from the textarea value
+    const lines = textArea.value.split("\n").length;
+
+    // Clear existing line number elements
+    lineCountElement.innerHTML = "";
+
+    // Create and append line number divs
+    for (let i = 1; i <= lines; i++) {
+        const lineNumberDiv = document.createElement("div");
+        lineNumberDiv.textContent = i;
+        lineCountElement.appendChild(lineNumberDiv);
+    }
+}
+//function for run button
+function executeCommands() {
+    rst_All_variable()
+    const regex = /module\s+(\w+)\s*\(/;
+    const match = regex.exec(textArea.value);
+
+    if (match) {
+        const module_name = match[1];
+        if (module_name == "BAI1" || module_name == "BAI2A"
+            || module_name == "BAI3A" || module_name == "BAI4A"
+            || module_name == "BAI5A" || module_name == "BAI6A") {
+
+            complie_code(module_name);
+        } else {
+            output.innerHTML = "";
+            output.innerHTML = `The module name: ${module_name} is not recognized`;
+            return;
+        }
+    } else {
+        output.innerHTML = "";
+        output.innerHTML = "No module name is recognized";
+        return;
+    }
+}
+
+// function for compling verilog code
+function complie_code(module_name) {
+
+    clearInterval(Io_computing);
+    let count = 0;
+    const lines_with_whitespace = textArea.value.split('\n');
+    const lines = textArea.value.replace(/ /g, '').split('\n');
+    //BAI1
+
+    if (module_name == "BAI1") {
+
+        for (let i = 0; i < lines.length; i++) {
+            const currentLine = lines[i];
+
+            // Loop through each character in the line
+            for (let j = 0; j < currentLine.length; j++) {
+                const char = currentLine[j];
+
+                if (char !== BAI1[count]) {
+
+                    output.innerHTML = "";
+                    output.innerHTML = `Module name: ${module_name}<br>
+                        NameError: "${char}", in line ${i + 1}<br>
+                        ${lines_with_whitespace[i]}`
+                    return;
+                }
+                count++;
+            }
+        }
+        if (count < BAI1.length) {
+            output.innerHTML = "";
+            output.innerHTML = `Module name: ${module_name}<br>
+                        Expected  "${BAI1[count]}" at the end <br>
+                        `
+            return;
+        }
+
+        output.innerHTML = "";
+        output.innerHTML = `Compile successfully module "${module_name}"`
+        // after 0.1s call function to computing output
+        Io_computing = setInterval(function () { verilog(module_name) }, 100)
+    }
+    //BAI2A 
+    else if (module_name == "BAI2A") {
+        for (let i = 0; i < lines.length; i++) {
+            const currentLine = lines[i];
+
+            // Loop through each character in the line
+            for (let j = 0; j < currentLine.length; j++) {
+                const char = currentLine[j];
+
+                if (char !== BAI2A[count]) {
+
+                    output.innerHTML = "";
+                    output.innerHTML = `Module name: ${module_name}<br>
+                        NameError: "${char}", in line ${i + 1}<br>
+                        ${lines_with_whitespace[i]}`
+                    return;
+                }
+                count++;
+            }
+        }
+        if (count < BAI2A.length) {
+            output.innerHTML = "";
+            output.innerHTML = `Module name: ${module_name}<br>
+                        Expected  "${BAI2A[count]}" at the end <br>
+                        `
+            return;
+        }
+        output.innerHTML = "";
+        output.innerHTML = `Compile successfully module "${module_name}"`
+        // after 0.1s call function to computing output
+        Io_computing = setInterval(function () { verilog(module_name) }, 100)
+    }
+    //BAI3A
+    else if (module_name == "BAI3A") {
+        for (let i = 0; i < lines.length; i++) {
+            const currentLine = lines[i];
+
+            // Loop through each character in the line
+            for (let j = 0; j < currentLine.length; j++) {
+                const char = currentLine[j];
+
+                if (char !== BAI3A[count]) {
+
+                    output.innerHTML = "";
+                    output.innerHTML = `Module name: ${module_name}<br>
+                        NameError: "${char}", in line ${i + 1}<br>
+                        ${lines_with_whitespace[i]}`
+                    return;
+                }
+                count++;
+            }
+        }
+        if (count < BAI3A.length) {
+            output.innerHTML = "";
+            output.innerHTML = `Module name: ${module_name}<br>
+                        Expected  "${BAI3A[count]}" at the end <br>
+                        `
+            return;
+        }
+        output.innerHTML = "";
+        output.innerHTML = `Compile successfully module "${module_name}"`
+        // after 0.1s call function to computing output
+        Io_computing = setInterval(function () { verilog(module_name) }, 100)
+    }
+    //BAI4A
+    else if (module_name == "BAI4A") {
+        for (let i = 0; i < lines.length; i++) {
+            const currentLine = lines[i];
+
+            // Loop through each character in the line
+            for (let j = 0; j < currentLine.length; j++) {
+                const char = currentLine[j];
+
+                if (char !== BAI4A[count]) {
+
+                    output.innerHTML = "";
+                    output.innerHTML = `Module name: ${module_name}<br>
+                        NameError: "${char}", in line ${i + 1}<br>
+                        ${lines_with_whitespace[i]}`
+                    return;
+                }
+                count++;
+            }
+        }
+        if (count < BAI4A.length) {
+            output.innerHTML = "";
+            output.innerHTML = `Module name: ${module_name}<br>
+                        Expected  "${BAI4A[count]}" at the end <br>
+                        `
+            return;
+        }
+        output.innerHTML = "";
+        output.innerHTML = `Compile successfully module "${module_name}"`
+        // after 0.1s call function to computing output
+        Io_computing = setInterval(function () { verilog(module_name) }, 100)
+    }
+    //BAI5A
+    else if (module_name == "BAI5A") {
+        for (let i = 0; i < lines.length; i++) {
+            const currentLine = lines[i];
+
+            // Loop through each character in the line
+            for (let j = 0; j < currentLine.length; j++) {
+                const char = currentLine[j];
+
+                if (char !== BAI5A[count]) {
+
+                    output.innerHTML = "";
+                    output.innerHTML = `Module name: ${module_name}<br>
+                        NameError: "${char}", in line ${i + 1}<br>
+                        ${lines_with_whitespace[i]}`
+                    return;
+                }
+                count++;
+            }
+        }
+        if (count < BAI5A.length) {
+            output.innerHTML = "";
+            output.innerHTML = `Module name: ${module_name}<br>
+                        Expected  "${BAI5A[count]}" at the end <br>
+                        `
+            return;
+        }
+        output.innerHTML = "";
+        output.innerHTML = `Compile successfully module "${module_name}"`
+        // after 0.1s call function to computing output
+        Io_computing = setInterval(function () { verilog(module_name) }, 100)
+    }
+    //BAI6A
+    else if (module_name == "BAI6A") {
+        for (let i = 0; i < lines.length; i++) {
+            const currentLine = lines[i];
+
+            // Loop through each character in the line
+            for (let j = 0; j < currentLine.length; j++) {
+                const char = currentLine[j];
+
+                if (char !== BAI6A[count]) {
+
+                    output.innerHTML = "";
+                    output.innerHTML = `Module name: ${module_name}<br>
+                        NameError: "${char}", in line ${i + 1}<br>
+                        ${lines_with_whitespace[i]}`
+                    return;
+                }
+                count++;
+            }
+        }
+        if (count < BAI6A.length) {
+            output.innerHTML = "";
+            output.innerHTML = `Module name: ${module_name}<br>
+                        Expected  "${BAI6A[count]}" at the end <br>
+                        `
+            return;
+        }
+        output.innerHTML = "";
+        output.innerHTML = `Compile successfully module "${module_name}"`
+        // after 0.1s call function to computing output
+        Io_computing = setInterval(function () { verilog(module_name) }, 100)
+
+    }
+}
+
 //select class push button
 const buttons = document.querySelectorAll(".button");
 
@@ -43,57 +319,13 @@ document.addEventListener("keyup", function (event) {
     }
 });
 
-// related to uploading file
-const fileInput = document.getElementById('file-upload');
-const uploadBtn = document.getElementById('upload-btn');
-const fileLabel = document.getElementById('file-label');
 
-//tile of file uploaded and initial empty string
-var name_module = "";
 
-//event click on label with class file upload
-uploadBtn.addEventListener('click', function () {
-    fileInput.click();
-});
-
-// event change on label with class file upload
-fileInput.addEventListener('change', function (event) {
-    const reader = new FileReader();
-    reader.onload = function (e) {
-        const content = e.target.result; //get all contents in file uploaded
-
-        // Regular expression to match a word after whitespace and before "("
-        const regex = /module\s+(\w+)\s*\(/;
-
-        // Match the string and capture the word in a group
-        const match = regex.exec(content);
-
-        if (match) {
-            // Extracted word is in the first capturing group (index 1)
-            const extractedWord = match[1];
-            //assign name of module
-            name_module = extractedWord;
-            rst_All_variable();
-            console.log("Extracted characters:", extractedWord); // Output: "name of module"
-        } else {
-            name_module = "-------";
-        }
-    };
-    reader.readAsText(event.target.files[0]);
-
-    // Update label text with filename
-    const fileName = event.target.files[0].name;
-    fileLabel.textContent = `Selected file: ${fileName}`;
-});
-
-// switch
-const switchInputs = document.querySelectorAll('.switch-input')
-
-//led
-const ledOutput = document.querySelectorAll('.led')
 
 //sign from switch
-var switch_input = [];
+let switch_input = [];
+
+
 //--------------------------------------------------------------------------------------------//
 //global variable for BAI4A
 let pre_clk_BAI4A = 0;
@@ -123,6 +355,12 @@ function rst_All_variable() {
     n_state = 0
     pre_clk_BAI6A = 0;
     w = 0
+    for (let i = 0; i < switchInputs.length; i++) {
+        switchInputs[i].checked = false;
+    }
+    for (let i = 0; i < ledOutput.length; i++) {
+        ledOutput[i].classList.remove["active_led"];
+    }
 }
 //-------------------------------------------------------------------------------------------//
 //object A
@@ -222,9 +460,6 @@ var A = {
         LED[0] = w;
         return LED;
     },
-    //rst = PB[0]
-    //clk = PB[1]
-    //w = sw[0]
 
 };
 
@@ -262,6 +497,8 @@ var B = {
         return this.LED;
     }
 };
+
+
 //object C
 var C = {
     SW: [],
@@ -283,11 +520,9 @@ var C = {
 
     }
 };
-var count = 1;
+
 // create a function to get and pass input, output 
-function verilog() {
-
-
+function verilog(name_module) {
     if (name_module != "") {
         // check switch on UI is checked or not
 
@@ -306,14 +541,10 @@ function verilog() {
         for (let i = 0; i < ledOutput.length; i++) {
             ledOutput[i].classList.toggle('active_led', C.LED[i] == 1);
         }
-        // console.log("rst---", "clk-----", "value");
-        console.log(C.SW[0] + "-----" + C.PB[0] + "----" + C.PB[1] + "-----" + p_state + "------" + n_state + "-------" + C.LED[0]);
 
     }
 
 }
-// initial function to get and pass input, output 
 
 
-// after 0.5s call function verilog
-setInterval(verilog, 100)
+
